9a896f9a63293f7b1815a8705390b6e0ffa004ac
maxime beauge <maxime.beauge@gmail.com>
2023-03-12 22:13:26 +0100

    config crud articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -18,15 +20,12 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
      flash[:notice] = "l'Article a été mis à jour"
      redirect_to article_path(@article)
@@ -36,9 +35,19 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article was deleted"
+    redirect_to articles_path
+   end
+
  private
 
   def article_params
     params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
